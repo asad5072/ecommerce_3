d705a590707f7cf15edb7c5201c6c4b142d05935
asad5072 <mmonir5072@gmail.com>
2025-03-06 15:36:04 +0600

    updated

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,14 +1,52 @@
 <template>
-    <div>
-        {{products}}
-        <p>Cart Page</p>
-        <div v-for="product in products" :key="product.id">
-            <p>Title: {{product.title}}</p>
-        </div>
+  <div>
+    {{ products }}
+    <p>Cart Page</p>
+    <div v-for="product in products" :key="product.id">
+      <p>Title: {{ product.title }}</p>
+      <div class="flex rounded-md overflow-hidden w-32">
+        <UButton
+          @click="decreaseCartItem(product.id)"
+          label="-"
+          color="white"
+          class="border"
+          :ui="{
+            rounded: 'rounded-none rounded-s-md',
+          }"
+        />
+        <UInput
+          type="text"
+          size="md"
+          placeholder=""
+          v-model="product.count"
+          class="border border-l-0 border-r-0 ring-0 text-black"
+          :ui="{
+            base: 'text-center',
+            rounded: 'rounded-none',
+            color: {
+              white: {
+                outline: 'ring-0',
+              },
+            },
+          }"
+        />
+        <UButton
+          @click="increaseCartItem(product.id)"
+          label="+"
+          color="gray"
+          class="border"
+          :ui="{
+            rounded: 'rounded-none rounded-e-md',
+          }"
+        />
+      </div>
     </div>
+    
+      
+  </div>
 </template>
 <script setup>
-    const store = useCartStore()
-    const {products} = storeToRefs(store);
-    // console.log(products, "asdfasd")
+const store = useCartStore();
+const { products } = storeToRefs(store);
+// console.log(products, "asdfasd")
 </script>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-        <div class="flex items-center justify-between gap-8 py-12">
+  <div>
+    <div class="flex items-center justify-between gap-8 py-12">
+        <div class="w-1/3 bg-slate-100 p-4 rounded">
+            <NuxtImg v-if="product.image" :src=product.image :alt=product.title class="w-full" />
+        </div>
+        <div class="w-2/3 flex flex-col gap-4">
             <p>Title: Product Title</p>
-
+            {{ product }}
+            <button @click="handleAddtoCart(product, 1)" class="w-full max-w-48 text-center bg-purple-200 rounded py-1">add to cart</button>
         </div>
     </div>
+  </div>
 </template>
+
 <script setup>
-    const {id} = useRoute().params
-    console.log(id, 'asad')
+const { id } = useRoute().params;
+console.log(id, "asad");
+const product = ref([""]);
+const res = await $fetch(`https://fakestoreapi.com/products/${id}`);
+product.value = res;
+const {addProduct}=useCartStore()
+// console.log(products, "asad");
+function handleAddtoCart(product, count){
+    // console.log(product, count);
+    addProduct(product,count)
+    
+}
 </script>
